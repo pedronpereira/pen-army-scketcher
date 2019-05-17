b79ebb2af7a725401339f71bd39aa7d9c084cfda
Pedro Pereira <pedronprpereira@gmail.com>
2019-05-17 15:57:49 +0100

    Add Warscroll

---
+++ src/PeNArmyScketcher.AgeOfSigmar/Warscroll.cs
@@ -0,0 +1,12 @@
+﻿namespace pen_data_parser
+{
+    public class Warscroll
+    {
+        public string Name { get; set; }
+        public int UnitSizeMin { get; set; }
+        public int UnitSizeMax { get; set; }
+        public int Points { get; set; }
+        public string Role { get; set; }
+        public string Notes { get; set; }
+    }
+}
